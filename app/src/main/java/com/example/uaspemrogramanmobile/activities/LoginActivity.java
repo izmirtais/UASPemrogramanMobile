88392b4d939dc2305d6f46a3e1b7045b75b58077
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-22 22:50:03 +0700

    menambahkan kode baru pada LoginActivity

--- app/src/main/java/com/example/uaspemrogramanmobile/activities/LoginActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobile/activities/LoginActivity.java
@@ -1,4 +1,16 @@
 package com.example.uaspemrogramanmobile.activities;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uaspemrogramanmobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
