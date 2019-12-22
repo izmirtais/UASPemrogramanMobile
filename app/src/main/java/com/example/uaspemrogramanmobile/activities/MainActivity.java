9e09a48fe65487de42195f324b3c8f9d58724e85
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-22 22:50:33 +0700

    mengaktifkan button Login

--- app/src/main/java/com/example/uaspemrogramanmobile/activities/MainActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobile/activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.uaspemrogramanmobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.uaspemrogramanmobile.R;
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
