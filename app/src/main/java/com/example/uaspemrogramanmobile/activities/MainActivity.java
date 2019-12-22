73adb153ec7efa5393570a64f85d992c23e9e3da
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-22 22:20:38 +0700

    memindahkan MainActivity dan membuat Class SplashActivity

--- app/src/main/java/com/example/uaspemrogramanmobile/activities/MainActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobile/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.uaspemrogramanmobile;
+package com.example.uaspemrogramanmobile.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.uaspemrogramanmobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
