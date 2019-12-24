ad97d6aac6867ac24375fd84fce93057ad670748
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-24 21:17:09 +0700

    memperbaharui file java class RegistrasiActivity

--- app/src/main/java/com/example/uaspemrogramanmobile/activities/RegistrasiActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobile/activities/RegistrasiActivity.java
@@ -1,4 +1,17 @@
 package com.example.uaspemrogramanmobile.activities;
 
-public class RegistrasiActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uaspemrogramanmobile.R;
+
+public class RegistrasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
 }
+
