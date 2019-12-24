d8619fc493887eaab7f12bdf1130ee77bc69a29a
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-24 22:36:04 +0700

    Halaman mulai selesai

--- app/src/main/java/com/example/uaspemrogramanmobile/activities/MulaiActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobile/activities/MulaiActivity.java
@@ -1,4 +1,17 @@
 package com.example.uaspemrogramanmobile.activities;
 
-public class MulaiActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.uaspemrogramanmobile.R;
+
+public class MulaiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mulai);
+    }
 }
+
