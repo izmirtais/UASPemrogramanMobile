3ada1a43e7b78ecebf8cae071d126ed02062b7ff
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2019-12-24 21:13:41 +0700

    mengedit dan menambahkan beberepa kode

--- app/src/main/java/com/example/uaspemrogramanmobile/activities/MainActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobile/activities/MainActivity.java
@@ -20,4 +20,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    public void handleRegister(View view) {
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
+    }
 }
